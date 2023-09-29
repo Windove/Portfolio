8335bb0ed8f68bafac60793ed5d6cfc892c4c253
Lukas <lukas@thrane.name>
2023-09-29 02:49:36 +0200

    planets and onclick events

--- portfolio/src/pages/projects.js
+++ portfolio/src/pages/projects.js
@@ -19,7 +19,7 @@ const Icosahedron = ({ rotationSpeed = -0.25 }) => {
 
     return (
         <mesh ref={ref}>
-            <icosahedronGeometry args={[1, 1]} />
+            <icosahedronGeometry args={[2, 1]} />
             <meshBasicMaterial wireframe color={color} />
         </mesh>
     );
@@ -55,12 +55,52 @@ const Star = ({ initialPosition, randomRadius, speed }) => {
     );
 };
 
+// A single planet
+const Planet = ({ initialPosition, speed, planetColor }) => {
+    const ref = useRef();
+    const time = useRef(0);  // Using useRef to persist state across renders
+
+    // Calculate initial angle
+    const initialAngle = Math.atan2(initialPosition.z, initialPosition.x);
+
+    useFrame((state, delta) => {
+        if (ref.current) {
+            const radius = initialPosition.length();
+            const adjustedSpeed = speed;
+            time.current += delta * adjustedSpeed;
+            const angle = time.current + initialAngle;
+            const y = initialPosition.y;
+            const x = radius * Math.cos(angle);
+            const z = radius * Math.sin(angle);
+            ref.current.position.set(x, y, z);
+        }
+    });
+
+    const handleOnClick = () => {
+        // Handle the click event here
+        event.stopPropagation();
+        console.log("Planet clicked!");
+    };
+
+    return (
+        <mesh ref={ref} position={initialPosition} onClick={handleOnClick}
+            onPointerOver={() => (document.body.style.cursor = 'pointer')}
+            onPointerOut={() => (document.body.style.cursor = 'auto')}
+        >
+            <sphereGeometry args={[0.5, 32, 32]} />
+            <meshBasicMaterial color={planetColor} />
+        </mesh>
+    );
+};
+
+
 // The scene
 const Scene = ({ starAmount }) => {
     const { camera } = useThree();
     const [scrollProgress, setScrollProgress] = useState(0);
 
     /*
+    // Load the HDRI texture
     const { scene } = useThree();
     useEffect(() => {
         new RGBELoader().load("/images/HDRI/hdr.hdr", function (texture) {
@@ -72,7 +112,7 @@ const Scene = ({ starAmount }) => {
     }, [scene]);
     */
 
-    // Keep track of the initial stars' positions and speeds
+    // Keep track of the initial stars positions and speeds
     const starInputValues = {
         speedMin: 0.25, speedMax: 1,
         radiusMin: 0.02, radiusMax: 0.07,
@@ -92,7 +132,7 @@ const Scene = ({ starAmount }) => {
                 degreesToRadians(starInputValues.angleYMax),
                 Math.random()
             );
-            const xAngle = degreesToRadians(360) * Math.random();            
+            const xAngle = degreesToRadians(360) * Math.random();
             const position = new THREE.Vector3().setFromSphericalCoords(distance, yAngle, xAngle);
 
             const speed = mix(starInputValues.speedMin, starInputValues.speedMax, Math.random());
@@ -101,11 +141,11 @@ const Scene = ({ starAmount }) => {
         }
 
         // create stationary stars far away in a circle around the center for background effect
-        for (let i = 0; i < 1000; i++) {
+        for (let i = 0; i < 500; i++) {
             const distance = mix(50, 500, Math.random());
             const yAngle = mix(
-                degreesToRadians(0),
-                degreesToRadians(360),
+                degreesToRadians(60),
+                degreesToRadians(180),
                 Math.random()
             );
             const xAngle = degreesToRadians(360) * Math.random();
@@ -115,18 +155,16 @@ const Scene = ({ starAmount }) => {
             newStarsData.push({ position: position.clone(), randomRadius, speed });
         }
 
-
         setStarsData(newStarsData);
     }, []);
 
-    // Update the stars whenever scrollProgress changes
+    // Update the stars
     const [stars, setStars] = useState([]);
     useEffect(() => {
         setStars(starsData.map((starData, i) => (
             <Star key={i} initialPosition={starData.position} randomRadius={starData.randomRadius} speed={starData.speed} />
         )));
     }, [starsData]);
-
 
     // User-Controlled Scroll
     useEffect(() => {
@@ -153,21 +191,27 @@ const Scene = ({ starAmount }) => {
 
     // Update the camera position based on scroll progress
     useFrame(() => {
-        const distance = 20; // Keeping distance constant
+        const distance = 30; // Keeping distance constant
 
         // During user-controlled scroll, interpolate the angle on the Y-axis for rotation
         const angleY = degreesToRadians(360 * 10) * scrollProgress;
-        camera.position.setFromSphericalCoords(distance, degreesToRadians(75), angleY);
+        camera.position.setFromSphericalCoords(distance, degreesToRadians(80), angleY);
 
         camera.updateProjectionMatrix();
         camera.lookAt(0, 0, 0);
     });
+
+    // Planet position
+    const planetPosition = new THREE.Vector3().setFromSphericalCoords(20, degreesToRadians(90), degreesToRadians(0));
+    const planetPosition2 = new THREE.Vector3().setFromSphericalCoords(20, degreesToRadians(90), degreesToRadians(180));
 
     // Return the scene
     return (
         <>
             <ambientLight />
             <Icosahedron rotationSpeed={-0.25} />
+            <Planet initialPosition={planetPosition} speed={0.1} planetColor={"blue"} />
+            <Planet initialPosition={planetPosition2} speed={0.1} planetColor={"red"} />
             {stars}
         </>
     );
@@ -190,15 +234,16 @@ const projects = () => {
                 <meta name='description' content='any description' />
             </Head>
 
-            <Canvas style={{ position: 'fixed', top: 0, left: 0, bottom: 0, right: 0, zIndex: -1, backgroundColor: "#000000" }}>
+            <Canvas style={{ position: 'fixed', top: 0, left: 0, bottom: 0, right: 0, zIndex: 1, backgroundColor: "#000000" }}>
                 <Scene starAmount={200} />
             </Canvas>
 
-            <main ref={containerRef} className='w-full h-[10000vh] bg-transparent'>
+            <main ref={containerRef} style={{ pointerEvents: 'none' }} className='w-full h-[10000vh] bg-transparent'>
             </main>
         </>
     );
 };
+
 
 
 export default projects;
